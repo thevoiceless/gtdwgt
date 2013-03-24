060ded598344b057da3406f8bb7fbd47bd1ba002
Riley Moses <victi.resurgo@gmail.com>
2013-03-24 12:44:01 -0600

    Enforce email uniqueness in rails and at database level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password_digest
+
+  # Convert email address to lowercase before saving it to the database so that the uniqueness constraint
+  # used by the index is guaranteed to work correctly
+  # Explanation for this caveat can be found at http://ruby.railstutorial.org/chapters/modeling-users#sec-uniqueness_validation
+  before_save { |user| user.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@gmail.com\z/i
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
