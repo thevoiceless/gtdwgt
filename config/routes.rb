88a61ce302cd6fb0c9e54a2b23d0ab03790d0512
Riley Moses <victi.resurgo@gmail.com>
2013-03-24 14:23:24 -0600

    Create signup route, redirect to it

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Gtdwgt::Application.routes.draw do
 
   resources :users
 
+  match '/signup',  to: 'users#new'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
