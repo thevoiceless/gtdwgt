9373f812a4accde8b9992dbf172c27bae9f01a1c
Riley Moses <victi.resurgo@gmail.com>
2013-03-24 14:43:21 -0600

    Implement user creation with secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest
+  attr_accessible :email, :password, :password_confirmation
+  has_secure_password
 
   # Convert email address to lowercase before saving it to the database so that the uniqueness constraint
   # used by the index is guaranteed to work correctly
@@ -20,5 +21,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@gmail.com\z/i
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
 end
