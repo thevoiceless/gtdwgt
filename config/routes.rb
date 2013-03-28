df9a41f6444e81117d04252e5e13919163c26a9a
Riley Moses <victi.resurgo@gmail.com>
2013-03-28 11:26:30 -0600

    Generate Sessions controller, create sign in/out routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Gtdwgt::Application.routes.draw do
   get "static_pages/home"
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
 
   # The priority is based upon order of creation:
