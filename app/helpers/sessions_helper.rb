902a9e399e62d4ad4a54b14efb477deecfc6a91b
Riley Moses <victi.resurgo@gmail.com>
2013-03-29 14:06:27 -0600

    Define methods for current_user in SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+	def sign_in(user)
+		# "Permanent" = Expires 20 years from now
+		cookies.permanent[:remember_token] = user.remember_token
+		self.current_user = user
+	end
+
+	# Must define assignment function for current_user, I have no idea why
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		# Calls find_by_remember_token the first time current_user is called
+		# On subsequent invocations returns @current_user without hitting the database
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
 end
