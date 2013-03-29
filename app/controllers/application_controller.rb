a50c80ea27c9a086b483fcb3a085f9e50776e52f
Riley Moses <victi.resurgo@gmail.com>
2013-03-29 13:50:08 -0600

    Include SessionsHelper in ApplicationController, add remember_token to DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # All helpers are available in views by default, but not in the controllers
+  # To use the methods from the Sessions helper in both places, include it explicitly
+  include SessionsHelper
+
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+  	sign_out
+  	super
+  end
 end
