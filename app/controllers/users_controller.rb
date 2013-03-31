bf8dbea992152b2ccb54bb4e4e397bc50973218b
Riley Moses <victi.resurgo@gmail.com>
2013-03-30 19:04:10 -0600

    Only view all users when signed in, show authorized actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update, :destroy]
 
   # GET /users
