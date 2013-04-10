683d12872854be7e69459e8cba107eb90e7ada5f
Riley Moses <victi.resurgo@gmail.com>
2013-04-09 19:11:13 -0600

    Complete WAG at how to utilize Tasks API in /lib

--- lib/gtasks_api.rb
+++ lib/gtasks_api.rb
@@ -2,36 +2,40 @@
 
 require 'google/api_client'
 
-# Initialize the client and Tasks API
-client = Google::APIClient.new(application_name: "GTDWGT Test", application_version: 1)
+class GTasksAPI
+	def initialize
+		@@client = Google::APIClient.new(application_name: "GTDWGT", application_version: 1)
+		@@client.authorization.client_secret = 'IKvOgAZlsnBmSRc9t3p6IebY'
+		@@client.authorization.redirect_uri = 'http://localhost:3000/oauth2callback'
+		@@client.authorization.scope = 'https://www.googleapis.com/auth/tasks'
 
-# Initialize OAuth 2.0 client    
-client.authorization.client_id = '158458358460.apps.googleusercontent.com'
-client.authorization.client_secret = 'IKvOgAZlsnBmSRc9t3p6IebY'
-client.authorization.redirect_uri = 'http://localhost:3000/oauth2callback'
-client.authorization.scope = 'https://www.googleapis.com/auth/tasks'
+		@@gtasks = @@client.discovered_api('tasks')
+	end
 
-gtasks = client.discovered_api('tasks')
+	def redirect_uri
+		@@client.authorization.authorization_uri
+	end
 
-# Print authorization URI, which must be manually opened in a browser
-redirect_uri = client.authorization.authorization_uri
-puts redirect_uri
+	def fetch_access_token
+		@@client.authorization.fetch_access_token!
+	end
 
-# After authorization, copy/paste authorization code from browser
-client.authorization.code = gets.chomp
-client.authorization.fetch_access_token!
+	def task_lists
+		# Get task lists
+		result = @@lient.execute(:api_method => @@gtasks.tasklists.list)
+		# Lists are in result.data.items
+		@lists = result.data.items
+		@lists
+	end
 
-# Get task lists
-result = client.execute(:api_method => gtasks.tasklists.list)
-# Lists are in result.data.items
-lists = result.data.items
-lists.each do |list|
-	# Get tasks for each list
-	result = client.execute(:api_method => gtasks.tasks.list, parameters: { 'tasklist' => list.id })
-	tasks = result.data.items
-	puts "#{list.title} - #{tasks.size} tasks"
-	# Display each task and its status
-	tasks.each do |task|
-		puts "\t#{task.title} (#{task.status})"
+	def tasks
+		# Get task lists if we haven't already
+		# TODO: Should task lists be re-fetched every time?
+		task_lists if @lists.nil?
+		@tasks = Hash.new
+		@lists.each do |list|
+			result = @@client.execute(:api_method => @@gtasks.tasks.list, parameters: { 'tasklist' => list.id })
+			@tasks[list] = result.data.items
+		end
 	end
 end
