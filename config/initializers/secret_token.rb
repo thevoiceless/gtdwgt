89ed32b7cf2a98060fc5467a9fe9d331775e5fe4
Riley Moses <victi.resurgo@gmail.com>
2013-04-19 14:23:32 -0600

    Re-add secret_token.rb, hopefully temporary

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtdwgt::Application.config.secret_token = '120cbed3b7c4fbcda56a013b2305352dcc0d7acbfb75771828fd1ac669715eba01fbb63a49227e0a018a5b228a97791e380827cae9e21a1bd2065a7bd806323b'
