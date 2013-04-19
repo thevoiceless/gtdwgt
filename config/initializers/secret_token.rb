2b3216d449ed78e4a0fcede85b1be2c44bc59400
Riley Moses <victi.resurgo@gmail.com>
2013-04-19 14:30:20 -0600

    Use different secret tokens in production and development

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Gtdwgt::Application.config.secret_token = '120cbed3b7c4fbcda56a013b2305352dcc0d7acbfb75771828fd1ac669715eba01fbb63a49227e0a018a5b228a97791e380827cae9e21a1bd2065a7bd806323b'
+Gtdwgt::Application.config.secret_token = ENV['SECRET_TOKEN'] || '120cbed3b7c4fbcda56a013b2305352dcc0d7acbfb75771828fd1ac669715eba01fbb63a49227e0a018a5b228a97791e380827cae9e21a1bd2065a7bd806323b'
